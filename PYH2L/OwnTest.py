8892dd5cf7b20e69b96e86e9ddc92523273d09ba
Josh Porter <36802302+jccport@users.noreply.github.com>
2018-06-13 15:54:20 +0100

    Added saving of coordinates for all boxes within a given image into a folder within the repository. Also moved the test images into the repository.

--- PYH2L/OwnTest.py
+++ PYH2L/OwnTest.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from matplotlib.widgets import RectangleSelector
 
 """
@@ -12,34 +13,46 @@ be drawn and the coordinates of two opposite corners of the rectangle (not sure
 which ones) are printed.
 """
 
-# TODO: Create function to write the coordinates as a .csv file with a name linking it to its associated image file.
 
-os.chdir("C:\\Users\Josh\Desktop\TestSet")
+def func(filename):
+    # Test images are now within repository so this line will set the correct directory as long as your local version is
+    # up to date
+    local_repo_path = os.getcwd()
+    os.chdir("Data/TestImages")
+    # Reading the image as an array.
+    img = cv2.imread("{}.png".format(filename))
 
-# Reading the image as an array.
-img = cv2.imread("Layer 2.png")
+    # Splitting the colour channels of the original image into separate arrays.
+    b, g, r = cv2.split(img)
 
-# Splitting the colour channels of the original image into separate arrays.
-b, g, r = cv2.split(img)
+    # Using fig, ax to make the interactive bit work.
+    fig, ax = plt.subplots()
+    plt.imshow(r)
 
-# Using fig, ax to make the interactive bit work.
-fig, ax = plt.subplots()
-plt.imshow(r)
+    coords = pd.DataFrame(columns=['blx', 'bly', 'trx', 'try'])
+
+    ix = 0
+
+    def line_select_callback(eclick, erelease, ix=ix):
+        x1, y1 = eclick.xdata, eclick.ydata
+        x2, y2 = erelease.xdata, erelease.ydata
+
+        rect = plt.Rectangle((min(x1, x2), min(y1, y2)), np.abs(x1 - x2), np.abs(y1 - y2))
+        coords.at[ix, 'blx'] = int(x1)
+        coords.at[ix, 'bly'] = int(y1)
+        coords.at[ix, 'trx'] = int(x2)
+        coords.at[ix, 'try'] = int(y2)
+        ix += 1
+        ax.add_patch(rect)
+
+    rs = RectangleSelector(ax, line_select_callback,
+                           drawtype='box', useblit=False, button=[1],
+                           minspanx=5, minspany=5, spancoords='pixels',
+                           interactive=True)
+
+    plt.show()
+    os.chdir(local_repo_path + '\Data\TestCoords')
+    coords.to_csv('{}.csv'.format(filename))
 
 
-def line_select_callback(eclick, erelease):
-    x1, y1 = eclick.xdata, eclick.ydata
-    x2, y2 = erelease.xdata, erelease.ydata
-
-    rect = plt.Rectangle((min(x1, x2), min(y1, y2)), np.abs(x1 - x2), np.abs(y1 - y2))
-    BoxCoords = [[int(x1), int(y1)], [int(x2), int(y2)]]
-    print(BoxCoords)
-    ax.add_patch(rect)
-
-
-rs = RectangleSelector(ax, line_select_callback,
-                       drawtype='box', useblit=False, button=[1],
-                       minspanx=5, minspany=5, spancoords='pixels',
-                       interactive=True)
-
-plt.show()
+func('Layer 2')
