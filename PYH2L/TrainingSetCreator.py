5a15fcf966c2ddaf6dec3ac7c5644928a0bd8a04
Josh Porter <jccporter@gmail.com>
2018-06-14 19:31:45 +0100

    Improved TrainingSetCreator.py to prevent accidental overwriting of coordinate data

--- PYH2L/TrainingSetCreator.py
+++ PYH2L/TrainingSetCreator.py
@@ -7,6 +7,7 @@ import pandas as pd
 from matplotlib.widgets import RectangleSelector
 
 plt.switch_backend('QT5Agg')
+positive_response = ['Yes', 'yes', 'y', 'yeah', 'Yeah', 'Y']
 
 """
 This script allows the reading of the test images as arrays, splits the colour
@@ -23,40 +24,49 @@ def func(filename):
     # up to date
     local_repo_path = os.getcwd()
     os.chdir("Data/TestImages")
-    # Reading the image as an array.
-    img = cv2.imread("{}.png".format(filename))
 
-    # Using fig, ax to make the interactive bit work.
-    fig, ax = plt.subplots()
-    plt.imshow(img)
+    def func2(filename=filename):
+        # Reading the image as an array.
+        img = cv2.imread("{}.png".format(filename))
 
-    coords = pd.DataFrame(columns=['blx', 'bly', 'trx', 'try'])
+        # Using fig, ax to make the interactive bit work.
+        fig, ax = plt.subplots()
+        plt.imshow(img)
 
-    def line_select_callback(eclick, erelease):
-        global ix
-        ix += 1
-        x1, y1 = eclick.xdata, eclick.ydata
-        x2, y2 = erelease.xdata, erelease.ydata
+        coords = pd.DataFrame(columns=['blx', 'bly', 'trx', 'try'])
 
-        rect = plt.Rectangle((min(x1, x2), min(y1, y2)), np.abs(x1 - x2), np.abs(y1 - y2))
-        coords.at[ix, 'blx'] = int(x1)
-        coords.at[ix, 'bly'] = int(y1)
-        coords.at[ix, 'trx'] = int(x2)
-        coords.at[ix, 'try'] = int(y2)
-        print(ix)
-        ax.add_patch(rect)
+        def line_select_callback(eclick, erelease):
+            global ix
+            ix += 1
+            x1, y1 = eclick.xdata, eclick.ydata
+            x2, y2 = erelease.xdata, erelease.ydata
 
-    rs = RectangleSelector(ax, line_select_callback,
-                           drawtype='box', useblit=False, button=[1],
-                           minspanx=5, minspany=5, spancoords='pixels',
-                           interactive=True)
+            rect = plt.Rectangle((min(x1, x2), min(y1, y2)), np.abs(x1 - x2), np.abs(y1 - y2))
+            coords.at[ix, 'blx'] = int(x1)
+            coords.at[ix, 'bly'] = int(y1)
+            coords.at[ix, 'trx'] = int(x2)
+            coords.at[ix, 'try'] = int(y2)
+            print(ix)
+            ax.add_patch(rect)
 
-    fig_manager = plt.get_current_fig_manager()
-    fig_manager.window.showMaximized()
-    plt.show()
-    os.chdir(local_repo_path + '\Data\TestCoords')
-    print(coords)
-    coords.to_csv('{}.csv'.format(filename), index=False)
+        rs = RectangleSelector(ax, line_select_callback,
+                               drawtype='box', useblit=False, button=[1],
+                               minspanx=5, minspany=5, spancoords='pixels',
+                               interactive=True)
+
+        fig_manager = plt.get_current_fig_manager()
+        fig_manager.window.showMaximized()
+        plt.show()
+        os.chdir(local_repo_path + '\Data\TestCoords')
+        print(coords)
+        coords.to_csv('{}.csv'.format(filename), index=False)
+
+    if filename + '.csv' in os.listdir(local_repo_path + "\Data\TestCoords"):
+        check = input("Coordinate data for this image is already present. Overwrite pre-existing coordinates?")
+        if check in positive_response:
+            return func2()
+    else:
+        return func2()
 
 
 func('Layer 2')
