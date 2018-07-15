e1bd478c36f7a8e3943dbfc4577f44781c4b7221
Josh Porter <jccporter@gmail.com>
2018-07-15 13:54:09 +0100

    Added some commenting to make the code easier to follow.
    Signed-off-by: Josh Porter <jccporter@gmail.com>

--- PYH2L/BBox_Test.py
+++ PYH2L/BBox_Test.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -9,7 +8,7 @@ from keras.layers import Flatten, Activation
 from keras.constraints import maxnorm
 from keras.optimizers import SGD
 from keras.layers.convolutional import Conv2D, Convolution2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import MaxPooling1D
 from keras.utils import np_utils
 from keras import backend as K
 K.set_image_dim_ordering('th')
@@ -24,10 +23,19 @@ X_train, y_train, X_test, y_test = CDRT.prepare_data()
 # print('X_train:', X_train, '\n\n\n', 'y_train:', y_train, '\n\n\n', 'X_test', X_test, '\n\n\n', 'y_test', y_test, '\n')
 
 # print(type(X_train))
+"""
+Before reshaping the dimensions of the arrays are (6, 313, 1055) and (6, 4, 11). The extra dimension is added as keras requires input of the form 
+(Number of images, image width, image height, number of channels), where number of channels refers to colour channels (RGB etc.), however, out images are 
+greyscale so contain only one channel.
+"""
 X_train = np.reshape(X_train, (6, 313, 1055, 1))
 X_test = np.reshape(X_test, (6, 313, 1055, 1))
-y_train = np.reshape(y_train, (6, 4, 11, 1))
-y_test = np.reshape(y_test, (6, 4, 11, 1))
+"""
+I'm rounding the coords purely incase they have been mutated again somehow. The img arrays are being divided by 255 and then rounded so that the images 
+contain pixels that are either black or white (0 or 1... possibly other way round, not sure).
+"""
+y_train = abs(np.round(np.reshape(y_train, (6, 4, 11, 1)), 0))
+y_test = abs(np.round(np.reshape(y_test, (6, 4, 11, 1)), 0))
 X_train = abs(np.round(X_train // 255, 0))
 X_test = abs(np.round(X_test // 255, 0))
 print('-----------REFORMATTED DATA-----------')
@@ -42,23 +50,24 @@ print('X_train:', X_train, '\n\n\n', 'y_train:', y_train, '\n\n\n', 'X_test', X_
 filter_size = 3
 pool_size = 2
 
-# TODO: Maybe remove pooling bc it takes away the spatial information.
-X_train = (X_train.reshape(6, -1) - np.mean(X_train)) / np.std(X_train)
-X_test = (X_test.reshape(6, -1) - np.mean(X_test)) / np.std(X_test)
-y_train = (y_train.reshape(6, -1) - np.mean(y_train)) / np.std(y_train)
-y_test = (y_test.reshape(6, -1) - np.mean(y_test)) / np.std(y_test)
+"""
+I don't fully understand why this needs to be done, but one of the tutorials did something similar, and adding this step stopped the code from crashing at 
+the model.compile stage. This is probably the right thing to do, however I am not sure that the arguments of .reshape() are the ones we should be using.
+"""
+X_train = ((X_train.reshape(6, -1) - np.mean(X_train)) / np.std(X_train)).astype("uint8")
+X_test = ((X_test.reshape(6, -1) - np.mean(X_test)) / np.std(X_test)).astype("uint8")
+y_train = ((y_train.reshape(6, -1) - np.mean(y_train)) / np.std(y_train)).astype("bool")
+y_test = ((y_test.reshape(6, -1) - np.mean(y_test)) / np.std(y_test)).astype("bool")
 
 model = Sequential([
     Dense(X_train.shape[-1]*4, input_dim=X_train.shape[-1]),
     Activation('relu'),
-    Dropout(0.9),
+    Dropout(0.2),
     Dense(y_train.shape[-1])
     ])
 model.compile('adadelta', 'mse')
 
 print(model.summary())
-
-# y = y_train.reshape(6, -1) / img_size
 
 
 with tf.device('/gpu:0'):
--- PYH2L/CollectedData_ReadingTest.py
+++ PYH2L/CollectedData_ReadingTest.py
@@ -1,18 +1,46 @@
 import numpy as np
 import pandas as pd
 from skimage.transform import resize
-test = pd.read_csv("C:\\Users\Josh\IdeaProjects\H2L\PYH2L\Data\MergeDat\Ting.csv", engine='c')
+import os
+print(os.getcwd())
+# If your project is contained in the same type of IdeaProjects form as mine, this file path should work.
+test = pd.read_csv("Data\MergeDat\Ting.csv", engine='c')
 test = test.drop(['Unnamed: 2'], axis=1).set_index(['Unnamed: 0', 'Unnamed: 1']).rename_axis(('Source',
                                                                                               'Type'))
+
+
 def rebin(a, shape):
-    sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
+    """
+
+    :param a: Array to reshape
+    :type a: np.array or other array object
+    :param shape: Dimensions of new array
+    :type shape: tuple
+    :return: Array with the dimensions: shape
+    :rtype:
+    """
+    sh = shape[0], a.shape[0]//shape[0], shape[1], a.shape[1]//shape[1]
     return a.reshape(sh).mean(-1).mean(1)
 
+
 def extract_data(layer, keepna):
+    """
+
+    :param layer: The level (image/coordinate set) of the MultiIndex 'test' to process.
+    :type layer: int
+    :param keepna: Whether to drop the empty cells to the right of a given layer or to keep them for the purpose of having all levels have the same dimensions.
+    :type keepna: bool
+    :return: Array of img data, array of coordinate data.
+    :rtype: np.array
+    """
     layers = test.index.levels[0]
     layer_name = layers[layer]
     Img = test.loc[(slice(None), 'Img'), :]
     Coords = test.loc[(slice(None), 'Coords'), :]
+    """
+    There are lots of steps here, none of them are particularly exciting. I don't think that I can give any more clarity in what each step does than you 
+    would get from printing each step.
+    """
     if not keepna:
         Img_Array_Shape_Mask = Img.xs(layer_name).isna()
         Img_Shape = Img_Array_Shape_Mask[~Img_Array_Shape_Mask].dropna(axis=1).shape
@@ -22,7 +50,6 @@ def extract_data(layer, keepna):
         Coords_Array = Coords.groupby(level=0).apply(lambda x: x.values.tolist()).values
         Imgs_Array = [np.extract(np.logical_not(np.isnan(Imgs_Array[i])), Imgs_Array[i]) for i in range(0, len(Imgs_Array))]
         Coords_Array = [np.extract(np.logical_not(np.isnan(Coords_Array[i])), Coords_Array[i]) for i in range(0, len(Coords_Array))]
-        # print(np.array(Imgs_Array[layer]), '\n\n')
         Oriented_Img_Data = np.array(np.fliplr(np.rot90(np.array(Imgs_Array[layer]).reshape(Img_Shape).astype("uint8"), 3)))
         Shaped_Coords = np.array(np.array(Coords_Array[layer]).reshape(Coords_Shape))
         return Oriented_Img_Data, Shaped_Coords
@@ -32,13 +59,18 @@ def extract_data(layer, keepna):
         Coords_Shape = Coords_Array_Shape_Mask[~Coords_Array_Shape_Mask].dropna(axis=1).shape
         Imgs_Array = Img.groupby(level=0).apply(lambda x: x.values.tolist()).values
         Coords_Array = Coords.groupby(level=0).apply(lambda x: x.values.tolist()).values
-        # print(np.array(Imgs_Array[layer]), '\n\n')
         Coords_Array = [np.extract(np.logical_not(np.isnan(Coords_Array[i])), Coords_Array[i]) for i in range(0, len(Coords_Array))]
-        Oriented_Img_Data = np.array(np.fliplr(np.rot90(np.array(Imgs_Array[layer]).reshape(Img_Shape).astype("uint8"), 3)))
+        Oriented_Img_Data = np.array(np.fliplr(np.rot90(np.array(Imgs_Array[layer]).reshape(Img_Shape).astype(
+                "uint8"), 3)))
         Shaped_Coords = np.array(np.array(Coords_Array[layer]).reshape(Coords_Shape))
+        print('SHAPED COORDS', Shaped_Coords)
         return Oriented_Img_Data, Shaped_Coords
 
 
+"""
+Creating empty lists to use in prepare_data. As they are resized to have uniform dimensions the list can be converted to a multidimension numpy array as 
+supposed to a numpy array containing array objects.
+"""
 train_imgs = []
 train_coords = []
 test_imgs = []
@@ -46,6 +78,12 @@ test_coords = []
 
 
 def prepare_data():
+    """
+    Resizes all img arrays to have the same dimensions, similarly for all coord arrays. Appends the returned data of extract_data for each layer contained in
+    the dataset.
+    :return: Separate training and test arrays for both imgs and coords.
+    :rtype: np.array
+    """
     global train_imgs, test_imgs, train_coords, test_coords
     max_length = 0
     max_coord_length = 0
@@ -58,12 +96,19 @@ def prepare_data():
         img[np.isnan(img)] = 0
         print('IMAGE SHAPE', img.shape)
         print('COORD SHAPE', coord.shape)
+        """
+        Checks if the current img and coord array of the loop is the largest array so far, if so notes the length (for img data) and width (for coord data). 
+        The fact that length is used for img data and width is used for coord data is fairly arbitrary and very much a result of this being a work in 
+        progress. I'm currently thinking that shaping each array to be a square with the lengths equal to the largest dimension (either length or width) of 
+        any of the arrays will be best for ensuring that keras can read the data easily. However, this will increase the size of the tensors and there are 
+        probably more efficient ways to shape the arrays.
+        """
         if img.shape[1] >= max_length:
             max_length = img.shape[1]
         if coord.shape[0] >= max_coord_length:
             max_coord_length = coord.shape[0]
         train_imgs.append(np.array(img))
-        train_coords.append(np.array(coord))
+        train_coords.append(coord)
     for m in test_index:
         img, coord = extract_data(m, True)
         coord[np.isnan(coord)] = 0
@@ -75,8 +120,13 @@ def prepare_data():
         if coord.shape[0] >= max_coord_length:
             max_coord_length = coord.shape[0]
         test_imgs.append(np.array(img))
-        test_coords.append(np.array(coord))
+        test_coords.append(coord)
+    print('TRAIN COORDS, PRE-RE-SIZING', train_coords)
     print('MAXIMUM LENGTHS', max_length, max_coord_length)
+    """
+    Here skimage.transform.resize is used to carry out the transformation. The documentation for this function is online and should provide all necessary 
+    insight into the function arguments used. List comprehension is used to iterate over each layer within the new array of arrays.
+    """
     train_imgs = [resize(train_imgs[n], (313, max_length), mode='constant', cval=0) for n in range(0, len(train_imgs))]
     test_imgs = [resize(test_imgs[n], (313, max_length), mode='constant', cval=0) for n in range(0, len(test_imgs))]
     train_coords = [resize(train_coords[n], (4, max_coord_length), mode='constant', cval=0) for n in range(0,
@@ -85,11 +135,16 @@ def prepare_data():
                                                                                                       len(test_coords))]
     # train_coords = [resize(train_coords[n], (313, max_length), mode='constant', cval=0) for n in range(0, len(train_coords))]
     # test_coords = [resize(test_coords[n], (313, max_length), mode='constant', cval=0) for n in range(0, len(test_coords))]
-    print('train_imgs', np.array(train_imgs), '\n')
-    print('train_coords', np.array(train_coords), '\n')
-    print('test_imgs', np.array(test_imgs), '\n')
-    print('test_coords', np.array(test_coords), '\n')
-    return np.array(train_imgs), np.array(train_coords), np.array(test_imgs), np.array(test_coords)
+    """
+    I am rounding to reduce space. However, it is odd that at this point the arrays contain floating point values as all of the data should be integers.
+    """
+    train_imgs2 = np.round(np.array(train_imgs), 2)
+    train_coords2 = np.round(np.array(train_coords), 0)
+    test_imgs2 = np.round(np.array(test_imgs), 2)
+    test_coords2 = np.round(np.array(test_coords), 0)
+    print('train_imgs', train_imgs2, '\n')
+    print('train_coords', train_coords2, '\n')
+    print('test_imgs', test_imgs2, '\n')
+    print('test_coords', test_coords2, '\n')
+    return train_imgs2, train_coords2, test_imgs2, test_coords2
 
-
-prepare_data()
